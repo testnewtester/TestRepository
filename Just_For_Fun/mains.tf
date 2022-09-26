ba3f649926826e49ac8795f0b0fac5f01f9c593c
testnewtester <112416957+testnewtester@users.noreply.github.com>
2022-09-26 19:05:18 +0530

    Commited

---
+++ Just_For_Fun/mains.tf
@@ -0,0 +1,8 @@
+
+resource"azurerm_linux_virtual_machine""Linux_VM4844010000" {
+
+name="joihuygftrdx" 
+location="kpoujhygftrx" 
+admin_username="uhygftdre" 
+network_interface_ids=["ojihuygftrhuygtfijhugyftx" ]
+}
